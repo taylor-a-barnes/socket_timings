a98982654d53c3a26e68c3bcd9ccd6669548ff2b
Taylor Barnes <taylor.a.barnes@gmail.com>
2018-10-26 10:26:05 -0400

    Changed LD_LIBRARY_PATH to relative path

--- tests/zmq/run.sh
+++ tests/zmq/run.sh
@@ -1,7 +1,7 @@
-LD_LIBRARY_PATH=/home/tbarnes/mdi/stemp/lib/zmq_build/lib/ ../../build/zmq_server &
+LD_LIBRARY_PATH=../../lib/zmq_build/lib/ ../../build/zmq_server &
 
 sleep 1
 
-LD_LIBRARY_PATH=/home/tbarnes/mdi/stemp/lib/zmq_build/lib/ ../../build/zmq_client &
+LD_LIBRARY_PATH=../../lib/zmq_build/lib/ ../../build/zmq_client &
 
 wait

7e0848fc670622877917e1cdb672e91edcf54af4
Taylor Barnes <tbarnes1@vt.edu>
2019-01-10 16:18:57 -0500

    Improve support when running without MPI

---
+++ dev/mac_build.sh
@@ -0,0 +1,28 @@
+#ensure that the intel compilers are being used
+CLOCATION="$(which gcc)"
+FLOCATION="$(which gfortran)"
+export CC="${CLOCATION}"
+export FC="${FLOCATION}"
+
+cd ../lib/
+rm -rf mdi_build
+mkdir mdi_build
+cd mdi_build
+cmake -Dmpi=OFF ../mdi
+make
+cp ../mdi/molssi_driver_interface/mdi.h molssi_driver_interface
+cd ../
+
+rm -rf zmq_build
+mkdir zmq_build
+cd zmq_build
+cmake ../libzmq
+make -j 4
+cd ../
+
+cd ../
+rm -rf build
+mkdir build
+cd build
+cmake ../socket_timings
+make
--- socket_timings/mpi_production.c
+++ socket_timings/mpi_production.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <mpi.h>
 #include "../lib/mdi_build/molssi_driver_interface/mdi.h"

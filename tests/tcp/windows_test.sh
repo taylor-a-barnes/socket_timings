8330b0101086c4632ccd636cf984414dad788ba6
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-07-15 14:11:03 -0400

    Add -Dlanguage cmake option

---
+++ tests/tcp/windows_test.sh
@@ -0,0 +1,7 @@
+
+echo "Running Windows Tests"
+
+# C - C
+../../build/mpi_driver -mdi "-role DRIVER -name driver -method TCP -port 8021" &
+../../build/mpi_production -mdi "-role ENGINE -name PONG -method TCP -hostname localhost -port 8021" &
+wait

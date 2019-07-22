2bdbd4c7c82d07a57f5e69ec5888d66bf9d963d3
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-07-22 09:56:48 -0400

    Update windows_test.sh

--- tests/tcp/windows_test.sh
+++ tests/tcp/windows_test.sh
@@ -2,6 +2,6 @@
 echo "Running Windows Tests"
 
 # C - C
-../../build/mpi_driver -mdi "-role DRIVER -name driver -method TCP -port 8021" &
-../../build/mpi_production -mdi "-role ENGINE -name PONG -method TCP -hostname localhost -port 8021" &
+..\..\build\mpi_driver -mdi "-role DRIVER -name driver -method TCP -port 8021" &
+..\..\build\mpi_production -mdi "-role ENGINE -name PONG -method TCP -hostname localhost -port 8021" &
 wait
